de1373ab1b8514e10726ec9140e9003b93ced5d5
Dario <dariocerosiete@gmail.com>
2025-10-06 14:25:50 +0200

    EJERCICIO5 ACABADO

--- practica02/src/ejercicio5.java
+++ practica02/src/ejercicio5.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class ejercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arrayInt = new int[10];
-        int i = (int) (Math.random() * 10 + 1);
+        int[] arrayInt = new int[9];
+        int i = (int) (Math.random() * 9 + 1);
         System.out.print("Numero aleatorio " + i + " ");
     }
     }
