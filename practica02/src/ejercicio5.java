1e02b975fa1f853bc47028cb74d7d25c1dc0ca6b
Dario <dariocerosiete@gmail.com>
2025-10-06 14:24:59 +0200

    EJERCICIO5 ACABADO

--- practica02/src/ejercicio5.java
+++ practica02/src/ejercicio5.java
@@ -4,12 +4,8 @@ public class ejercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arrayInt = new int[10];
-
-        System.out.println("Introduce valores enteros:");
-        for (int i = 0; i < arrayInt.length; i++) {
-            System.out.print("Valor " + i + " ");
-            arrayInt[i] = sc.nextInt();
-
-        }
+        int i = (int) (Math.random() * 10 + 1);
+        System.out.print("Numero aleatorio " + i + " ");
     }
-}
+    }
+
