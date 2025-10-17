de98f56c68382ffafe1a0140ae10581dca21a2e6
Dario <dariocerosiete@gmail.com>
2025-10-17 10:52:44 +0200

    EJERCICIO8 Empezado1

--- practica02/src/ejercicio8.java
+++ practica02/src/ejercicio8.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class ejercicio8 {
+    public static void main(String[] args) {
+        //Programa Java para leer la altura de N personas
+        //calcular la altura media
+        //Calcular cuántas personas tienen una altura superior a la media
+        //cuántas tienen una altura inferior a la media
+        //El valor de N se pide por teclado y debe ser entero positivo
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
+
+
