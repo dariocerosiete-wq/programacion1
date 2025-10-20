de7b8c63c48bc9c26d58285e333abac3fa770781
Dario <dariocerosiete@gmail.com>
2025-10-20 09:16:34 +0200

    EJERCICIO8 ACABADO

--- practica02/src/ejercicio8.java
+++ practica02/src/ejercicio8.java
@@ -9,7 +9,30 @@ public class ejercicio8 {
         //El valor de N se pide por teclado y debe ser entero positivo
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("¿Cuántas personas? ");
+        int N = sc.nextInt();
+
+        double[] alturas = new double[N];
+        double suma = 0;
+
+        for (int i = 0; i < N; i++) {
+            System.out.print("Altura " + (i + 1) + ": ");
+            alturas[i] = sc.nextDouble();
+            suma += alturas[i];
+        }
+
+        double media = suma / N;
+        System.out.println("Altura media: " + media);
+
+        int masAltos = 0;
+        int masBajos = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (alturas[i] > media) masAltos++;
+            if (alturas[i] < media) masBajos++;
+        }
+
+        System.out.println("Altura superior a la media: " + masAltos);
+        System.out.println("Altura inferior a la media: " + masBajos);
     }
 }
-
-
