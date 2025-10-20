d14ec4a83677e91af0d8ff6bb47c5901911d8888
Dario <dariocerosiete@gmail.com>
2025-10-20 12:35:48 +0200

    EJERCICIO 11 ACABADO

---
+++ practica02/src/ejercicio11.java
@@ -0,0 +1,16 @@
+public class ejercicio11 {
+    public static void main(String[] args) {
+                int[] numeros = {1, 2, 3, 4, 5, 6};
+
+                System.out.println("Array original:");
+                for (int i = 0; i < 6; i++) {
+                    System.out.println(numeros[i]);
+                }
+
+                System.out.println("Array invertido:");
+                for (int i = 5; i >= 0; i--) {
+                    System.out.println(numeros[i]);
+                }
+            }
+        }
+
