858b7cdd338e9e57259f5c72981f54194e4082dd
Dario <dariocerosiete@gmail.com>
2025-10-22 10:47:13 +0200

    EJERCICIO 12 ACABADO(1)

--- practica02/src/ejercicio12.java
+++ practica02/src/ejercicio12.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class ejercicio12 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        char[] letras = {
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'
-        };
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Escribe solo el numero de su DNI: ");
-        int numeroDNI = teclado.nextInt();
 
-        int resto = numeroDNI % 23;
+        System.out.print("Introduce el número del DNI: ");
+        int numero = sc.nextInt();
+
+
+        int resto = numero % 23;
+
 
         System.out.println("La letra del DNI es: " + letras[resto]);
+
+        sc.close();
     }
 }
