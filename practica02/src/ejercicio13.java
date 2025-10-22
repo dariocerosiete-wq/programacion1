5277549c660177f013105639a4ba615f34a54d54
Dario <dariocerosiete@gmail.com>
2025-10-22 09:42:00 +0200

    EJERCICIO 13 ACABADO1

--- practica02/src/ejercicio13.java
+++ practica02/src/ejercicio13.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class ejercicio13 {
     public static void main(String[] args) {
         {
+            //Cree una lista de enteros (ArrayList<Integer>).
             Scanner s = new Scanner(System.in);
             ArrayList<Integer> lista = new ArrayList<>();
-
+            //Pida números por teclado hasta que el usuario introduzca un número negativo (ese no se añade).
             System.out.println("Escribe números positivos. Si escribes uno negativo, se termina.");
 
             int numero = 0;
@@ -18,7 +19,7 @@ public class ejercicio13 {
                     lista.add(numero); // guardar en la lista
                 }
             }
-
+            //Muestre por pantalla todos los números de la lista y la suma total de los mismos.
             System.out.println("Los números que escribiste son:");
             int suma = 0;
 
