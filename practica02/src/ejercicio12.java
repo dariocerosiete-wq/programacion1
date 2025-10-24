7382dbdf9b4f775e7ef56ceaa7411654274b8ac6
Dario <dariocerosiete@gmail.com>
2025-10-24 09:54:28 +0200

    EJERCICIO 12 acabado con enunciado

--- practica02/src/ejercicio12.java
+++ practica02/src/ejercicio12.java
@@ -1,3 +1,7 @@
+//Pedir número DNI al usuario
+//Programa que te devuelva la letra
+//Resultado
+
 import java.util.Scanner;
 
 public class ejercicio12 {
@@ -7,14 +11,14 @@ public class ejercicio12 {
         char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
                 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
-
+        //Pedir número DNI al usuario
         System.out.print("Introduce el número del DNI: ");
         int numero = sc.nextInt();
 
-
+        //Programa que te devuelva la letra
         int resto = numero % 23;
 
-
+        //Resultado
         System.out.println("La letra del DNI es: " + letras[resto]);
     }
 }
