c39f3b53c8a702acb8f2d230df02178c044b21a0
Dario <dariocerosiete@gmail.com>
2025-10-24 09:56:14 +0200

    EJERCICIO 11 acabado con enunciado

--- practica02/src/ejercicio11.java
+++ practica02/src/ejercicio11.java
@@ -1,7 +1,11 @@
+//PRUEBA CON UN ARRAY DE TAMAÑO 6.
+//Programa java que invierta el orden de los valores de un array
+
 public class ejercicio11 {
     public static void main(String[] args) {
+                //PRUEBA CON UN ARRAY DE TAMAÑO 6.
                 int[] numeros = {1, 2, 3, 4, 5, 6};
-
+                //Programa java que invierta el orden de los valores de un array
                 System.out.println("Array original:");
                 for (int i = 0; i < 6; i++) {
                     System.out.println(numeros[i]);
