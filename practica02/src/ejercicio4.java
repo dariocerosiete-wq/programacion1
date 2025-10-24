f99818259df6f5cc8af2256d2debf6806d95d3c5
Dario <dariocerosiete@gmail.com>
2025-10-24 10:03:02 +0200

    EJERCICIO 4 acabado con enunciado

--- practica02/src/ejercicio4.java
+++ practica02/src/ejercicio4.java
@@ -1,5 +1,9 @@
+//Crea un array de números de 100 posiciones contendrá los números del 1 al 100
+//Obtén la suma de todos ellos y la media
+
 public class ejercicio4 {
     public static void main(String[] args) {
+        //Crea un array de números de 100 posiciones contendrá los números del 1 al 100
         int[] numeros = new int[100];
         int suma = 0;
         double media;
@@ -8,7 +12,7 @@ public class ejercicio4 {
             suma = suma + numeros[i];
         }
         media = (double) suma / 100;
-
+        //Obtén la suma de todos ellos y la media
         System.out.println("La suma de los números del 1 al 100 es: " + suma);
         System.out.println("La media de los números del 1 al 100 es: " + media);
     }
