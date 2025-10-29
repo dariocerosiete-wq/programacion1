2c7502323ec84fe0657d89d10c5a508c35ec6ed6
Dario <dariocerosiete@gmail.com>
2025-10-29 14:28:55 +0100

    ejercicio1 empezado

--- practica3/src/ejercicio1.java
+++ practica3/src/ejercicio1.java
@@ -1,2 +1,30 @@
 public class ejercicio1 {
+    public static void main(String[] args) {
+        // Define un array de números tipo double de 3 filas por 7 columnas con nombre doub
+        double[][] doub = {
+                //asigna los valores según la siguiente tabla
+                {0, 30, 2, 0, 0, 0, 5},
+                {75, 0, 0, 0, 0, 0, 0},
+                {0, 0,-2 , 0, 9, 0, 11}
+        };
+
+        // Imprimir encabezado
+        System.out.print("Array num  ");
+        for (int j = 0; j < 6; j++) {
+            System.out.printf("Columna %-3d ", j);
+        }
+        System.out.println();
+
+        // Imprimir filas
+        for (int i = 0; i < doub.length; i++) {
+            System.out.printf("Fila %-6d", i);
+            for (int j = 0; j < doub[i].length; j++) {
+                System.out.printf("%8.0f ", doub[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
+
+
+
