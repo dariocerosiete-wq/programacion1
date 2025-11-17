d3c9b61b6a378e9264989eecdbc63c9637f2f807
Dario <dariocerosiete@gmail.com>
2025-11-17 10:56:42 +0100

    Ejercicio ejemplo poo casi terminado

--- Practica_poo/src/Main.java
+++ Practica_poo/src/Main.java
@@ -1,13 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+// Parte 1
+        Usuario user = new Usuario("Dario", "Romero Romero", "11408", "C/Rodrigo de Jerez", "dariocerosiete@gmail.com", "04042007.d");
+        System.out.println(user.getNombre());
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        user.setNombre("Juanillo");
+
+        System.out.println(user.getNombre());
+
+        System.out.println(user.getEmail());
+
+        user.setEmail("dario@.com");
+
+        System.out.println(user.getEmail());
+
+        System.out.println(user.toString());
+
+        System.out.println(user.checkUsuario("dario@.com", "04042007.d"));
+
+        //Parte 2
+        Usuario user = new Usuario("Alejandro", "Romero Cuadrado", "11400", "C/Rodrigo de Cádiz", "alesietesiete@gmail.com", "04042007.dD");
+        Usuario user = new Usuario("Adrian", "Romero Gonzalez", "11401", "C/Rodrigo de Granada", "adriunosiete@gmail.com", "04042007.dDd");
+        Usuario user = new Usuario("Roberto", "Romero Paniagua", "11402", "C/Rodrigo de Mayo", "robertocerosiete@gmail.com", "04042007.ddd");
+
+        U
+
     }
 }
+
+
+
+
---
+++ Practica_poo/src/Usuario.java
@@ -0,0 +1,53 @@
+public class Usuario {
+    //Atributos
+    private String nombre;
+    private String apellidos;
+    private String codigoPostal;
+    private String direccion;
+    private String email;
+    private String contraseña;
+    //Contructor
+    public Usuario(String n, String a, String c , String d, String e, String co) {
+
+        this.nombre = n;
+        this.apellidos = a;
+        this.codigoPostal = c;
+        this.direccion = d;
+        this.email = e;
+        this.contraseña = co;
+    }
+//Metodo get y Set
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail (String nuevoEmail){
+        if (nuevoEmail.contains("@")){
+            this.email = nuevoEmail;
+        }
+        else {
+            System.out.println("Email incorrecto");
+        }
+    }
+
+    //metodo tostring
+    public String toString(){
+        return this.nombre + " " + this.apellidos + " " + this.codigoPostal + " " + this.email;
+    }
+
+    //checkusuario
+    public  Boolean checkUsuario (String email, String contraseña){
+        return this.getEmail().equals(email) && this.contraseña.equals(contraseña);
+    }
+
+}
+
+
