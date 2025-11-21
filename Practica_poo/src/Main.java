28e11b83c9daac38d964a5e48e45470e1c4a0bcb
Dario <dariocerosiete@gmail.com>
2025-11-21 09:13:55 +0100

    ejemplos poo casi acabado por el siete3

--- Practica_poo/src/Main.java
+++ Practica_poo/src/Main.java
@@ -24,16 +24,17 @@ public class Main {
         Usuario user4 = new Usuario("Roberto", "Romero Paniagua", "11402", "C/Rodrigo de Mayo", "robertocerosiete@gmail.com", "04042007.ddd");
 
         Usuario[] usuarios = {user1, user2, user4};
-
-
-
+        validarRegistro(usuarios);
+        user.1
+    }
+    public static void validarRegistro(Usuario[] usuarios())
         for (int i = 0; i < usuarios.length; i++) {
             System.out.println(usuarios[i].getNombre());
 
 
         }
     }
-}
+
 
 
 
