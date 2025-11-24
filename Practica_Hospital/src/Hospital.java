fea80ddb7701fd1c162a500ac48008349fda7fbd
Dario <dariocerosiete@gmail.com>
2025-11-24 13:48:14 +0100

    hospital por terminar

--- Practica_Hospital/src/Hospital.java
+++ Practica_Hospital/src/Hospital.java
@@ -5,9 +5,34 @@ public class Hospital {
     private String cif;
     private ArrayList<Area> lista;
 
-    public Hospital(String n,String C) {
+    public Hospital(String n, String C) {
         this.nombre = n;
         this.cif = C;
-        this.lista = ;
+        this.lista = new ArrayList<>();
+
+
     }
+
+    public String getNombre() {
+        return this.nombre;
+
+    }
+
+    public void setNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+
+    }
+
+    public String getCif() {
+        return this.cif;
+
+    }
+
+    public void setCif(String nuevoCif) {
+        this.cif = nuevoCif;
+
+    }
+
 }
+
+
