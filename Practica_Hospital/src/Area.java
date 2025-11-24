78219d1e9abf15892866663ce3a8c518e5ab8aa8
Dario <dariocerosiete@gmail.com>
2025-11-24 14:25:40 +0100

    area empezado

--- Practica_Hospital/src/Area.java
+++ Practica_Hospital/src/Area.java
@@ -1,2 +1,13 @@
 public class Area {
+    private String nombre;
+    private String identificador;
+    private int planta;
+    private Hospital Hospital;
+
+public Area(String n, String i, int p, Hospital h){
+    this.nombre = n;
+    this.identificador = i;
+    this.planta = p;
+    this.Hospital = h;
+}
 }
