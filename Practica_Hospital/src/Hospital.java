df5e9459bfc64281d29ba0e2792747ef25dfef0e
Dario <dariocerosiete@gmail.com>
2025-11-24 10:55:20 +0100

    Casi terminado y ficheros nuevos

---
+++ Practica_Hospital/src/Hospital.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Hospital {
+    private String nombre;
+    private String cif;
+    private ArrayList<Area> lista;
+
+    public Hospital(String n,String C) {
+        this.nombre = n;
+        this.cif = C;
+        this.lista = ;
+    }
+}
