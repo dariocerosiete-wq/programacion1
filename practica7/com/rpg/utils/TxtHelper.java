52d5f7594fbec5d2d7080ae40f035c928e642b11
dario <dariocerosiete@gmail.com>
2026-02-23 20:45:37 +0100

    gdehghadcdsa

--- practica7/com/rpg/utils/TxtHelper.java
+++ practica7/com/rpg/utils/TxtHelper.java
@@ -6,10 +6,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class TxtHelper {
-    public TxtHelper() {
+public TxtHelper(){
 
-    }
-
+}
     public static void main(String[] args) {
         try {
             List<String> lineas = Files.readAllLines(Paths.get("practica7\\ficheros\\ciudades.txt"));
